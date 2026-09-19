8b4ee03d3561b4e124ac4f23903b8ebf7239d1ec
blazusta <blazusta@gmail.com>
2026-09-19 14:39:55 +0100

    feat(member_moderation): add /timeout and /untimeout commands

--- cogs/member_moderation.py
+++ cogs/member_moderation.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from datetime import timedelta
 
 class MemberModeration(commands.Cog):
 
@@ -124,7 +125,7 @@ class MemberModeration(commands.Cog):
         
         if member.top_role >= interaction.user.top_role and interaction.user != interaction.guild.owner:
             await interaction.response.send_message(
-                "You cannot ban someone with a higher or equal to your highest role!",
+                "You cannot ban someone with a role higher than or equal to yours",
                 ephemeral=True
             )
             return
@@ -143,7 +144,7 @@ class MemberModeration(commands.Cog):
 
         try:
             await member.ban(
-                delete_message_days=delete_messages, 
+                delete_message_seconds=delete_messages * 86400, 
                 reason=f"Banned by {interaction.user.name}: {reason}"
             )
             await interaction.response.send_message(
@@ -171,8 +172,8 @@ class MemberModeration(commands.Cog):
 
 
     @app_commands.guild_only()
-    @app_commands.command(name="unban", description="unban a banned member")
-    @app_commands.describe(user_id = "The banned member ID to unban")
+    @app_commands.command(name="unban", description="Unban a banned member")
+    @app_commands.describe(user_id="The banned member ID to unban")
     @app_commands.checks.has_permissions(ban_members=True)
     async def unban_member(
         self, 
@@ -228,6 +229,170 @@ class MemberModeration(commands.Cog):
         if isinstance(error, app_commands.MissingPermissions):
             await interaction.response.send_message(
                 "You do not have the `Ban Members` permission to execute this command.",
+                ephemeral=True
+            )
+
+
+    @app_commands.guild_only()
+    @app_commands.command(name="timeout", description="Disallow a member from sending messages for a period")
+    @app_commands.describe(period="Period of timeout")
+    @app_commands.choices(period=[
+        app_commands.Choice(name="60 Seconds", value=60),
+        app_commands.Choice(name="5 Minutes", value=300),
+        app_commands.Choice(name="1 Hour", value=3600),
+        app_commands.Choice(name="1 Day", value=86400),
+        app_commands.Choice(name="1 Week", value=604800)
+    ])
+    @app_commands.checks.has_permissions(moderate_members=True)
+    async def timeout_member(
+        self, 
+        interaction: discord.Interaction, 
+        member: discord.Member,
+        period: app_commands.Choice[int],
+        reason: str = "No reason provided."
+    ):
+        
+        if member.bot:
+            await interaction.response.send_message(
+                "You cannot execute this command on bots!",
+                ephemeral=True
+            )
+            return
+        
+        if member == interaction.user:
+            await interaction.response.send_message(
+                "You cannot timeout yourself!",
+                ephemeral=True
+            )
+            return
+        
+        if member == interaction.guild.owner:
+            await interaction.response.send_message(
+                "You cannot timeout the server owner!",
+                ephemeral=True
+            )
+            return       
+        
+        if member.top_role >= interaction.user.top_role and interaction.user != interaction.guild.owner:
+            await interaction.response.send_message(
+                "You cannot timeout someone with a role higher than or equal to yours",
+                ephemeral=True
+            )
+            return
+        
+        if member.top_role >= interaction.guild.me.top_role:
+            await interaction.response.send_message(
+                "I cannot timeout this user because their role is higher than or equal to my highest role!",
+                ephemeral=True
+            )
+            return
+        
+        try:
+            duration = timedelta(seconds=period.value)
+            await member.timeout(duration, reason=reason)
+            await interaction.response.send_message(
+                f"{member.mention} has been disallowed to send messages for {period.name}\n**Reason:** {reason}",
+                ephemeral=True
+            )
+
+            try:
+                await member.send(f"You have been disallowed to send messages for {period.name}\n**Reason:** {reason}\n{member.mention}")
+            except (discord.Forbidden, discord.HTTPException):
+                pass
+
+        except discord.Forbidden:
+            await interaction.response.send_message(
+                "I do not have the permission to timeout this user.",
+                ephemeral=True
+            )
+        except discord.HTTPException as e:
+            await interaction.response.send_message(
+                f"Failed to timeout this user: {e}",
+                ephemeral=True
+            )
+
+    @timeout_member.error
+    async def timeout_member_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingPermissions):
+            await interaction.response.send_message(
+                "You do not have the `Moderate Members` permission to execute this command!",
+                ephemeral=True
+            )
+        
+
+    @app_commands.guild_only()
+    @app_commands.command(name="untimeout", description="Revoke the timeout from a member")
+    @app_commands.checks.has_permissions(moderate_members=True)
+    async def untimeout_member(
+        self, 
+        interaction: discord.Interaction,
+        member: discord.Member,
+        reason: str = "No reason provided."
+    ):
+        
+        if member.bot:
+            await interaction.response.send_message(
+                "You cannot execute this command on bots!",
+                ephemeral=True
+            )
+            return
+        
+        if not member.is_timed_out():
+            await interaction.response.send_message(
+                f"{member.mention} is not timed out!",
+                ephemeral=True
+            )
+            return
+        
+        if member == interaction.user:
+            await interaction.response.send_message(
+                "You cannot untimeout yourself!",
+                ephemeral=True
+            )
+            return
+        
+        if member.top_role >= interaction.user.top_role and interaction.user != interaction.guild.owner:
+            await interaction.response.send_message(
+                "You cannot untimeout this member because their role is higher than or equal to your highest role!",
+                ephemeral=True
+            )
+            return
+        
+        if member.top_role >= interaction.guild.me.top_role:
+            await interaction.response.send_message(
+                "I cannot untimeout this member because their role is higher than or equal to my highest role!",
+                ephemeral=True
+            )
+            return
+        
+        try:
+            await member.timeout(None, reason=reason)
+            await interaction.response.send_message(
+                f"{member.mention} has been allowed to send messages in this server\n**Reason:** {reason}",
+                ephemeral=True
+            )
+
+            try:
+                await member.send(f"You can now send messages in {interaction.guild.name}\n**Reason:** {reason}\n{member.mention}")
+            except (discord.Forbidden, discord.HTTPException):
+                pass
+
+        except discord.Forbidden:
+            await interaction.response.send_message(
+                "I do not have the permission to untimeout this user.",
+                ephemeral=True
+            )
+        except discord.HTTPException as e:
+            await interaction.response.send_message(
+                f"Failed to untimeout this user: {e}",
+                ephemeral=True
+            )
+
+    @untimeout_member.error
+    async def untimeout_member_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingPermissions):
+            await interaction.response.send_message(
+                "You do not have the `Moderate Members` permission to execute this command.",
                 ephemeral=True
             )
 
